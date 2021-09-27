a294e0f1d1c61ca3a39bc66d1b5929735c71f904
mnewlive <morozovvadim91@gmail.com>
2021-09-27 23:07:07 +0300

    Prepared navigation to our list screen, removed redundant composable components

--- app/src/main/java/com/saltedge/hackathon/MainActivity.kt
+++ app/src/main/java/com/saltedge/hackathon/MainActivity.kt
@@ -5,34 +5,39 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.saltedge.hackathon.presentation.Screen
+import com.saltedge.hackathon.presentation.account_list.components.AccountListScreen
 import com.saltedge.hackathon.ui.theme.HackathonTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             HackathonTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.AccountListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.AccountListScreen.route
+                        ) {
+                            AccountListScreen(navController)
+                        }
+//                        composable(
+//                            route = Screen.AccountDetailScreen.route + "/{accountId}"
+//                        ) {
+//                            AccountDetailScreen()
+//                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    HackathonTheme {
-        Greeting("Android")
     }
 }
